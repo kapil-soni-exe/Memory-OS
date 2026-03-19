ef1bd21cd39983240492f414cba72f10894dee1d
Kapil-coder <soni.kapil789@gmail.com>
2026-03-19 18:52:03 +0530

    File upload support

--- backend/routes/item.routes.js
+++ backend/routes/item.routes.js
@@ -2,10 +2,11 @@ import express from "express"
 import protect from "../middleware/auth.middleware.js"
 
 import { saveItem, getAllItems, getItem, deleteItem, updateItem } from "../controllers/item.controller.js"
+import upload from "../middleware/upload.middleware.js";
 
 const router = express.Router();
 
-router.post("/save", protect, saveItem);
+router.post("/save", protect, upload.single("file"), saveItem);
 router.get("/", protect, getAllItems);
 router.get("/:id", protect, getItem);
 router.put("/:id", protect, updateItem);
